2f6de384d1332d4f66997753368b960b097f8048
Shamim Keshani <sh.keshani@gmail.com>
2025-06-04 15:56:14 +0330

    fix sidebar layout for different sizes, pixel perfect

--- src/components/SidebarLayout.tsx
+++ src/components/SidebarLayout.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useTheme } from 'next-themes';
 import { Button } from "@/components/ui/button"
-import { Sheet, SheetTrigger, SheetContent, SheetClose } from "@/components/ui/sheet"
+import { Sheet, SheetTrigger, SheetContent, SheetClose, SheetHeader, SheetTitle, SheetDescription } from "@/components/ui/sheet"
 import { Menu, CheckCheck, Settings, CalendarHeart } from "lucide-react"
 import { Moon, Sun } from 'lucide-react';
 
@@ -23,59 +23,15 @@ export default function SidebarLayout({ children, onMenuClick, activeView }: { c
   ]
 
   return (
-    <div className="flex h-screen">
-      {/* Mobile Sidebar */}
-      <div className="md:hidden">
-        <Sheet open={open} onOpenChange={setOpen}>
-          <SheetTrigger asChild>
-            <Button variant="ghost" size="icon" className="m-2" onClick={() => setOpen(true)}>
-              <Menu />
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left" className="w-64 p-2">
-            <SheetClose>
-              <Button
-                variant="ghost"
-                size="default"
-                className="w-full justify-start"
-              >
-                <Menu />
-              </Button>
-            </SheetClose>
-            <div className="flex flex-col h-full space-y-2">
-              {menuItems.map(({ icon: Icon, label, onClick }) => {
-                if (!Icon) return (<div key={label} className="mt-auto" />)
-                else return (
-                  <Button
-                    key={label}
-                    variant="ghost"
-                    className="justify-start"
-                    onClick={() => {
-                      onClick();
-                      setOpen(false);
-                    }}
-                  >
-                    <Icon className="mr-2" /> {label}
-                  </Button>
-                )
-              })}
-            </div>
-            <p className="text-xs text-muted-foreground text-center">
-              &nbsp;{open && `v${__APP_VERSION__}`}
-            </p>
-          </SheetContent>
-        </Sheet>
-      </div>
-
-      {/* Desktop Sidebar */}
+    <div className="flex flex-row h-screen">
+      {/* Sidebar - Wide View (desktop or horizental mobile) */}
       <div
-        className={`hidden md:flex flex-col transition-all duration-300 bg-muted border-r
-          ${collapsed ? "w-14" : "w-64"}`}
+        className={`hidden sm:flex flex-col flex-none transition-all duration-300 border-r box-content ${collapsed ? "w-16" : "w-48"}`}
       >
         <Button
-          variant="shk"
+          variant="ghost"
           size="icon"
-          className="m-2 w-auto p-2"
+          className="w-auto m-2 p-2 justify-center"
           onClick={() => setCollapsed(!collapsed)}
         >
           <Menu />
@@ -88,7 +44,7 @@ export default function SidebarLayout({ children, onMenuClick, activeView }: { c
                 key={label}
                 variant="shk"
                 size="icon"
-                className={`m-2 w-auto p-2 ${collapsed ? "" : "justify-start"}`}
+                className={`w-auto m-2 p-2 ${collapsed ? "" : "justify-start"}`}
                 onClick={onClick}
               >
                 <Icon />{!collapsed && <span>{label}</span>}
@@ -101,13 +57,75 @@ export default function SidebarLayout({ children, onMenuClick, activeView }: { c
         </p>
       </div>
 
-      {/* Main Content */}
-      <div className="flex-1 overflow-y-auto p-4">
-        <div>
-          {activeView}
-        </div>
-        <div>
-          {children}
+      {/* Header + Content */}
+      <div className="flex flex-col">
+        {/* Header */}
+        <header className="flex h-13 items-center justify-left border-b box-content">
+          {/* Sheet open button (short width only) */}
+          <div className="sm:hidden">
+            <Sheet open={open} onOpenChange={setOpen}>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost" size="icon"
+                  className="p-2 m-2 w-12 justify-center"
+                  onClick={() => setOpen(true)}
+                >
+                  <Menu />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="w-64 p-2">
+                <SheetHeader className="hidden">
+                  <SheetTitle>Sheet Menu</SheetTitle>
+                  <SheetDescription>
+                    &nbsp;
+                  </SheetDescription>
+                </SheetHeader>
+                <SheetClose>
+                  <Button
+                    variant="ghost"
+                    size="default"
+                    className="w-full justify-start"
+                  >
+                    <Menu />
+                  </Button>
+                </SheetClose>
+                <div className="flex flex-col h-full space-y-2">
+                  {menuItems.map(({ icon: Icon, label, onClick }) => {
+                    if (!Icon) return (<div key={label} className="mt-auto" />)
+                    else return (
+                      <Button
+                        key={label}
+                        variant="ghost"
+                        className="justify-start"
+                        onClick={() => {
+                          onClick();
+                          setOpen(false);
+                        }}
+                      >
+                        <Icon className="mr-2" /> {label}
+                      </Button>
+                    )
+                  })}
+                </div>
+                <p className="text-xs text-muted-foreground text-center">
+                  &nbsp;{open && `v${__APP_VERSION__}`}
+                </p>
+              </SheetContent>
+            </Sheet>
+          </div>
+          <div className="p-2 border-s sm:border-none">
+            <span className="text-lg font-semibold">App Name</span>
+          </div>
+        </header>
+
+        {/* Main Content */}
+        <div className="flex-1 overflow-y-auto p-4">
+          <div>
+            {activeView}
+          </div>
+          <div>
+            {children}
+          </div>
         </div>
       </div>
     </div >
