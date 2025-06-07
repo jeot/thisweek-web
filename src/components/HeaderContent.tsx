013b7090d491901a8262bbf821c2327609c9dc3d
Shamim Keshani <sh.keshani@gmail.com>
2025-06-07 22:36:54 +0330

    make header pretty in mobile

--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -8,8 +8,8 @@ export default function HeaderContent({ title }: { title?: string }) {
   // console.log(today2);
 
   return (
-    <div className="h-full flex items-center p-2">
-      <h2 className="flex-1">{title}</h2>
+    <div className="h-full flex items-center justify-between px-2 py-1">
+      <h2 className="flex-none">{title}</h2>
       <div className="h-full flex flex-wrap items-center justify-end">
         <Badge variant="secondary" className="mx-1 font-normal" dir={today.dir}>{today.text}</Badge>
         <Badge variant="secondary" className="mx-1 font-light text-primary/75 bg-primary/10" dir={today.dir}>{today2.text}</Badge>
