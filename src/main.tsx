921459fc6734d5a3a08606a3058d8ac000105b38
Shamim Keshani <sh.keshani@gmail.com>
2025-07-12 19:12:09 +0330

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'next-themes';
 import App from './App.tsx'
 import { loadGA } from './ga';
 
+console.log("ga:", import.meta.env.VITE_GA_ID);
 // Load GA only in production and if ID is provided
 if (import.meta.env.PROD && import.meta.env.VITE_GA_ID) {
   console.log("loading GA...");
