ad9efa19cbcf419fd14370953cee7839037c9e04
Shamim Keshani <sh.keshani@gmail.com>
2025-08-07 22:48:44 +0330

    add route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from 'next-themes';
 import App from './App.tsx'
 import { loadGA } from './ga';
 import { AppInitializer } from './AppInitializer.tsx';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { LandingPage } from '@/LandingPage'
 
 console.log(`* ThisWeek App v${__APP_VERSION__}`);
 
@@ -19,7 +21,17 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <AppInitializer>
-        <App />
+        <BrowserRouter basename="/">
+          <Routes>
+            {/* Redirect / to /app */}
+            <Route path="/" element={<Navigate to="/app" replace />} />
+            {/* use this when your landing page gets ready: */}
+            {/* <Route path="/" element={<LandingPage />} /> */}
+
+            {/* Your main app route */}
+            <Route path="/app/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
       </AppInitializer>
     </ThemeProvider>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
