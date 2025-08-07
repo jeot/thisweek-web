799c9b79207544bf41231877a48a9d65f41aa2fe
Shamim Keshani <sh.keshani@gmail.com>
2025-08-07 22:10:10 +0330

    test /app

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/app/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
